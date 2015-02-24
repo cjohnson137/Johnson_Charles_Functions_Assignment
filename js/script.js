55ae6a31813b654945377f8abc62a718320d3ed4
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-24 01:29:05 -0600

    Adding Prompt
    Creating prompt and validation loop

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@
  * February 26, 2014
  */
 
+var lottoType = prompt ("Please enter Florida Lotto or Powerball.");
+
+    while (lottoType == ""){
+        prompt("Response can not be empty. Please enter Florida Lotto or Powerball.")
+    }
+
+
