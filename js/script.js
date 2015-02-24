95ea8888994b52a98f0fb13bec1c170a0c21365f
cobaltgalaxy <CJ-MACBOOK@Charless-MacBook-Pro.local>
2015-02-24 01:30:50 -0600

    Simplified Random Numbers
    Generating 5 random numbers 0 - 59

--- js/script.js
+++ js/script.js
@@ -11,4 +11,25 @@ var lottoType = prompt ("Please enter Florida Lotto or Powerball.");
         prompt("Response can not be empty. Please enter Florida Lotto or Powerball.")
     }
 
+function myLotto () {
 
+
+    var num1 = Math.floor(Math.random() * 60);
+    var num2 = Math.floor(Math.random() * 60);
+    var num3 = Math.floor(Math.random() * 60);
+    var num4 = Math.floor(Math.random() * 60);
+    var num5 = Math.floor(Math.random() * 60);
+
+
+
+    console.log("First Ball: " + num1);
+    console.log(num2);
+    console.log(num3);
+    console.log(num4);
+    console.log(num5);
+
+
+
+}
+
+myLotto();
